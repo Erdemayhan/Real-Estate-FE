ea5d3aa68f77d94c94fea71d6c11edd444e767c8
HazanSerifli9 <hazansherifli@gmail.com>
2024-01-28 15:55:04 +0200

    animated css

--- src/components/accountDetails/accountdetails.js
+++ src/components/accountDetails/accountdetails.js
@@ -5,34 +5,32 @@ import ChangePassword from "./ChangePassword";
 import SignOut from "./SignOut";
 import DeleteAccount from "./DeleteAccount";
 
-function AccountDetails() {
+const AccountDetails = () => {
   const tabs = [
     { name: "Personal Details", component: <Personal /> },
     { name: "Change Password", component: <ChangePassword /> },
     { name: "Sign Out", component: <SignOut /> },
     { name: "Delete Account", component: <DeleteAccount /> },
   ];
-  const [activeTab, setActiveTab] = useState(tabs[0]);
+
+  const [activeTab, setActiveTab] = useState(0);
+
   return (
     <div>
       <div className="profile-tabs">
-        {tabs.map((tab, index) => {
-          return (
-            <div
-              className={`col-2 py-4 ${
-                activeTab === index ? "active-tab" : ""
-              }`}
-              onClick={() => setActiveTab(index)}
-              key={tab.name}
-            >
-              {tab.name}
-            </div>
-          );
-        })}
+        {tabs.map((tab, index) => (
+          <div
+            className={`tab ${activeTab === index ? "active-tab" : ""}`}
+            onClick={() => setActiveTab(index)}
+            key={tab.name}
+          >
+            {tab.name}
+          </div>
+        ))}
       </div>
-      <div className="row-4">{tabs.at(activeTab).component}</div>
+      <div>{tabs[activeTab].component}</div>
     </div>
   );
-}
+};
 
 export default AccountDetails;
